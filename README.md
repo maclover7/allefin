0b84b99900a1a34a48ee9bd6027ef987767038c9
Jon Moss <me@jonathanmoss.me>
2023-05-01 17:30:48 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# allefin
+
+Small utility to list new documents filed with Allegheny County's
+campaign finance system.
+
+### Development
+
+- Run: `node index.js`
---
+++ index.js
@@ -0,0 +1,24 @@
+const { readFile, writeFile } = require('fs/promises');
+
+const getAllFilings = () => {
+  return fetch("https://documents.alleghenycounty.us/PAVNextGen/api/CustomQuery/KeywordSearch", {
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      Keywords: [{ ID: 393, Value: 2023, KeywordOperator: "=" }],
+      QueryID: 136
+    }),
+    method: "POST"
+  })
+    .then((r) => r.json())
+    .then((r) => Promise.resolve(r.Data.map((filing) => filing.Name)));
+};
+
+const getKnownFilings = () => readFile('filings.json').then(JSON.parse);
+
+Promise.all([getAllFilings(), getKnownFilings()])
+  .then(([filings, knownFilings]) => {
+    const newFilings = filings.filter((filing) => !knownFilings.includes(filing));
+    newFilings.forEach(console.log);
+
+    return writeFile('filings.json', JSON.stringify(filings.concat(newFilings)));
+  });
